5a843151d498ba801458fb7d9bb7a2ca600d5fc4
Grégor <gregordrebes@gmail.com>
2022-11-30 13:08:05 -0300

    Fix RoleController import

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Spatie\Permission\Models\Rent;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 use DB;
 
